6fee81ecdf725b0d0ae0b3620d645412861c319a
WideVader <sever.1.trifunovic@gmail.com>
2022-12-12 18:47:23 +0100

    Added comments to Lessons 13 and 14

--- Lessons/Lesson13_pointer_basics/main.c
+++ Lessons/Lesson13_pointer_basics/main.c
@@ -2,11 +2,17 @@
 
 int main(){
     printf("VARIABLE \t VALUE\n");
+    //Declaring variable number
     int number = 10;
+    //Printing value of number
     printf("number      \t %i \n", number);
+    //Printing adress of number by using pointer
     printf("&number      \t %p \n", &number);
 
+    //Declaring pointer variable with adress of number
     int* p_number = &number;
+    //Printing adress of pointer p_number
     printf("p_number       \t %p \n", p_number);
+    //Printing value of variable number
     printf("*p_number \t %i \n", *p_number);
 }
--- Lessons/Lesson14_max_pointers/main.c
+++ Lessons/Lesson14_max_pointers/main.c
@@ -1,21 +1,61 @@
 #include <stdio.h>
 
-int max(int a, int b);
+int* max(int* a, int* b);
 
 int main(){
+    //Declaring variables num1 and num2
     int num1 = 10;
     int num2 = 30;
-    int bigger = max(num1,num2);
-    printf("Bigger: %i\n", bigger);
-    num2 = 3;
-    printf("Bigger: %i\n", bigger);
+    //Printinf values of num1 and num2
+    printf("[main] num1: %i\n", num1);
+    printf("[main] num2: %i\n", num2);
+    //Printing adresses of num1 and num2
+    printf("[main] &num1: %p\n", &num1);
+    printf("[main] &num2: %p\n", &num2);
+
+    //Assigning pointer bigger result of function max
+    int* bigger = max(&num1, &num2);
+    printf("[main] bigger: %p\n", bigger);
+    printf("[main] *bigger: %i\n", *bigger);
 }
 
-int max(int a, int b){
-    if(a>b){
+int* max(int* a, int* b){
+    //Printing adresses of a and b
+    printf("[max] a: %p\n", a);
+    printf("[max] b: %p\n", b);
+    //Checking if pointer a or pointer b is bigger
+    if(*a>*b){
         return a;
     }
     else{
         return b;
     }
 }
+
+
+
+
+
+
+
+//This is to show how getting bigger value works
+
+// int max(int a, int b);
+
+// int main(){
+//     int num1 = 10;
+//     int num2 = 30;
+//     int bigger = max(num1,num2);
+//     printf("Bigger: %i\n", bigger);
+//     num2 = 3;
+//     printf("Bigger: %i\n", bigger);
+// }
+
+// int max(int a, int b){
+//     if(a>b){
+//         return a;
+//     }
+//     else{
+//         return b;
+//     }
+// }
